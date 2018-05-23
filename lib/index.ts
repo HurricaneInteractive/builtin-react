0f143205969edb6cafeab2944828faee4e42755a
Adro.Codes <hurricane.interactive@gmail.com>
2018-05-23 20:56:42 +1000

    Added type sizes to package

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,15 @@
 import * as colors from './styles/colors';
 
+import {
+    typeXXL,
+    typeXL,
+    typeL,
+    typeM,
+    typeN,
+    typeS,
+    typeXS
+} from './styles/typography';
+
 export { Button, ButtonLink } from './elements/Button';
 export { H1, H2, H3, H4 } from './elements/Headings';
 export { Text } from './elements/Type';
@@ -10,3 +20,4 @@ export { Pill, ButtonPill } from './elements/Pill';
 export { Alert } from './components/Alert';
 
 export const palette = {...colors};
+export const fontSizes = { typeXXL, typeXL, typeL, typeM, typeN, typeS, typeXS };
--- lib/styles/typography.ts
+++ lib/styles/typography.ts
@@ -7,3 +7,11 @@ export const blockquote = 20;
 export const button = 15;
 export const label = 15;
 export const tags = 12;
+
+export const typeXXL = '42px';
+export const typeXL = '35px';
+export const typeL = '28px';
+export const typeM = '21px';
+export const typeN = '16px';
+export const typeS = '14px';
+export const typeXS = '12px';
